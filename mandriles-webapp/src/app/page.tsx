2fdf742f876ac56ae7a1a9d105cac52ad0a1ff1d
Jairoalperez <jairoalberto98@hotmail.com>
2024-12-27 12:54:48 -0500

    updated home page

--- mandriles-webapp/src/app/page.tsx
+++ mandriles-webapp/src/app/page.tsx
@@ -1,11 +1,35 @@
-import MandrilList from "../components/MandrilList";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import MandrilCard from "../components/MandrilCard";
+import { cn } from "@components/lib/utils";
+import MandrilList from "@components/components/MandrilList";
 
 const Home: React.FC = () => {
-    return (
-        <div>
-            <MandrilList />
-        </div>
-    );
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const handleChange = (e: MediaQueryListEvent) => setIsDarkMode(e.matches);
+
+    setIsDarkMode(mediaQuery.matches);
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
+  return (
+    <body
+      className={cn(
+        "bg-background min-h-screen",
+        isDarkMode ? "dark" : "light"
+      )}
+    >
+      <div>
+        <MandrilList />
+      </div>
+    </body>
+  );
 };
 
 export default Home;
