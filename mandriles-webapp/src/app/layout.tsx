92ac3990eab99060d8d21b818abd9d5245fd24f7
Jairoalperez <jairoalberto98@hotmail.com>
2024-12-27 15:31:43 -0500

    added theme provider to layout

--- mandriles-webapp/src/app/layout.tsx
+++ mandriles-webapp/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ThemeProvider from "../helpers/ThemeProvider"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,10 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` min-h-screen bg-background ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+		<ThemeProvider />
         {children}
       </body>
     </html>
   );
 }
+
