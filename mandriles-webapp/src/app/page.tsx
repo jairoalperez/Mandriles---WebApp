fad66315da76d76c9c0e12771d3886a1978739eb
Jairoalperez <jairoalberto98@hotmail.com>
2024-12-27 14:02:10 -0500

    updated theme implementation

--- mandriles-webapp/src/app/page.tsx
+++ mandriles-webapp/src/app/page.tsx
@@ -1,35 +1,14 @@
-"use client";
+"use client"
 
-import React, { useEffect, useState } from "react";
-import MandrilCard from "../components/MandrilCard";
-import { cn } from "@components/lib/utils";
-import MandrilList from "@components/components/MandrilList";
+import React, { useEffect, useState } from "react"
+import MandrilList from "@components/components/MandrilList"
 
 const Home: React.FC = () => {
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+	return (
+		<div className="min-h-screen bg-background">
+			<MandrilList />
+		</div>
+	)
+}
 
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    const handleChange = (e: MediaQueryListEvent) => setIsDarkMode(e.matches);
-
-    setIsDarkMode(mediaQuery.matches);
-    mediaQuery.addEventListener("change", handleChange);
-
-    return () => mediaQuery.removeEventListener("change", handleChange);
-  }, []);
-
-  return (
-    <body
-      className={cn(
-        "bg-background min-h-screen",
-        isDarkMode ? "dark" : "light"
-      )}
-    >
-      <div>
-        <MandrilList />
-      </div>
-    </body>
-  );
-};
-
-export default Home;
+export default Home
--- mandriles-webapp/src/components/MandrilList.tsx
+++ mandriles-webapp/src/components/MandrilList.tsx
@@ -10,7 +10,6 @@ const MandrilList: React.FC = () => {
 	const [mandrils, setMandrils] = useState<Mandril[]>([])
 	const [loading, setLoading] = useState<boolean>(true)
 	const [error, setError] = useState<string | null>(null)
-	const [isDarkMode, setIsDarkMode] = useState<boolean>(false)
 
 	useEffect(() => {
 		let config = {
@@ -30,22 +29,6 @@ const MandrilList: React.FC = () => {
 			})
 	}, [])
 
-	useEffect(() => {
-		const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-		const handleChange = (e: MediaQueryListEvent) => {
-			setIsDarkMode(e.matches);
-			console.log('Dark mode:', e.matches);
-		};
-
-		// Set initial state
-		setIsDarkMode(mediaQuery.matches);
-		console.log('Initial dark mode:', mediaQuery.matches);
-		mediaQuery.addEventListener('change', handleChange);
-
-		// Clean up the event listener on component unmount
-		return () => mediaQuery.removeEventListener('change', handleChange);
-	}, []);
-
 	if (loading) {
 		return <div>Loading...</div>
 	}
@@ -56,18 +39,18 @@ const MandrilList: React.FC = () => {
 
 	return (
 		<div>
-		<div className={cn(isDarkMode ? "dark" : "", "flex flex-col items-center ml-8 mr-8 mt-8 p-4")}>
-			<h1 className="text-4xl font-bold mb-8 mt-8 text-foreground">MANDRILES</h1>
-			<div
-				className={cn(
-					"grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4 p-4",
-				)}
-			>
-				{mandrils.map((mandril) => (
-					<MandrilCard key={mandril.id} mandril={mandril} />
-				))}
+			<div className={cn("flex flex-col items-center ml-8 mr-8 mt-8 p-4")}>
+				<h1 className="text-4xl font-bold mb-8 mt-8 text-foreground">MANDRILES</h1>
+				<div
+					className={cn(
+						"grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4 p-4"
+					)}
+				>
+					{mandrils.map((mandril) => (
+						<MandrilCard key={mandril.id} mandril={mandril} />
+					))}
+				</div>
 			</div>
-		</div>
 		</div>
 	)
 }
