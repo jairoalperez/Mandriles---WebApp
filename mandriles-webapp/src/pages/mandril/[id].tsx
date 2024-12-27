d62d0f6d992df2cfad5de6d0a71c7fb99b440778
Jairoalperez <jairoalberto98@hotmail.com>
2024-12-27 13:24:31 -0500

    created all the routing logic to move between pages

---
+++ mandriles-webapp/src/pages/mandril/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MandrilDetail: React.FC = () => {
+    return (
+        <div>
+            <h1>Mandril Detail Page</h1>
+            {/* Details will go here */}
+        </div>
+    );
+};
+
+export default MandrilDetail;
