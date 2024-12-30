190dd4b69665972910f74f4c32bc34ccc0c45a69
Jairoalperez <jairoalberto98@hotmail.com>
2024-12-30 09:48:22 -0500

    created route to get new page of specific mandril

---
+++ mandriles-webapp/src/app/mandril/[id]/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import React from 'react';
+import { useParams, useRouter } from 'next/navigation';
+import axios from 'axios';
+import { cn } from '@components/lib/utils';
+
+const MandrilDetail: React.FC = () => {
+    const params = useParams(); // Get the params object from useParams
+    const id = params?.id; // Access the id parameter from the params object
+
+    const [mandril, setMandril] = React.useState(null);
+    const [loading, setLoading] = React.useState(true);
+    const [error, setError] = React.useState<string | null>(null);
+    const [isDarkMode, setIsDarkMode] = React.useState<boolean>(false)
+
+    React.useEffect(() => {
+        if (id) {
+            let config = {
+                method: "get",
+                url: `https://localhost:7095/mandril/${id}`,
+            };
+
+            axios
+                .request(config)
+                .then((response) => {
+                    setMandril(response.data);
+                    setLoading(false);
+                })
+                .catch((error) => {
+                    setError(`Error fetching data ${error}`);
+                    setLoading(false);
+                });
+        }
+    }, [id]);
+
+    React.useEffect(() => {
+        console.log(mandril);
+    }, [mandril]);
+
+    React.useEffect(() => {
+        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        const handleChange = (e: MediaQueryListEvent) => {
+            setIsDarkMode(e.matches);
+        };
+
+        setIsDarkMode(mediaQuery.matches);
+        mediaQuery.addEventListener('change', handleChange);
+
+        return () => mediaQuery.removeEventListener('change', handleChange);
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    return (
+        <div>
+            <div className={cn("flex flex-col items-center ml-8 mr-8 mt-8 p-4 border-solid border-2 border-primary")}>
+                <h1 className="text-4xl font-bold mb-8 mt-8 text-foreground">Details for Mandril</h1>
+            </div>
+        </div>
+    );
+};
+
+export default MandrilDetail;
--- mandriles-webapp/src/components/MandrilList.tsx
+++ mandriles-webapp/src/components/MandrilList.tsx
@@ -5,6 +5,7 @@ import axios from "axios"
 import { Mandril } from "../types/mandril"
 import { cn } from "@components/lib/utils"
 import MandrilCard from "./MandrilCard"
+import Link from 'next/link';
 
 const MandrilList: React.FC = () => {
 	const [mandrils, setMandrils] = useState<Mandril[]>([])
@@ -54,18 +55,20 @@ const MandrilList: React.FC = () => {
 
 	return (
 		<div>
-		<div className={cn("flex flex-col items-center ml-8 mr-8 mt-8 p-4")}>
+			<div className={cn("flex flex-col items-center ml-8 mr-8 mt-8 p-4")}>
 			<h1 className="text-4xl font-bold mb-8 mt-8 text-foreground">MANDRILES</h1>
-			<div
-				className={cn(
-					"grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4 p-4",
-				)}
-			>
-				{mandrils.map((mandril) => (
-					<MandrilCard key={mandril.id} mandril={mandril} />
+				<div
+					className={cn(
+						"grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4 p-4",
+					)}
+				>
+					{mandrils.map((mandril) => (
+					<Link key={mandril.id} href={`/mandril/${mandril.id}`}>  
+						<MandrilCard mandril={mandril} />
+					</Link>
 				))}
+				</div>
 			</div>
-		</div>
 		</div>
 	)
 }
