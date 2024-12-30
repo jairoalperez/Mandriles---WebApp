2500bbd8e49ed78bcc7c6b10fe344a679ac2fda8
Jairoalperez <jairoalberto98@hotmail.com>
2024-12-30 16:18:30 -0500

    added the clickable features to the card

--- mandriles-webapp/src/components/MandrilCreateCard.tsx
+++ mandriles-webapp/src/components/MandrilCreateCard.tsx
@@ -12,13 +12,18 @@ import {
 import React, { useEffect, useState } from "react";
 import { cn } from "@components/lib/utils";
 
-const MandrilCreateCard: React.FC = () => {
+interface MandrilCreateCardProps {
+  onClick?: () => void;
+}
+
+const MandrilCreateCard: React.FC<MandrilCreateCardProps> = ({ onClick }) => {
   return (
     <div className="w-[280px]">
       <Card
         className={cn(
           "text-center transition-colors duration-200 cursor-pointer bg-yellow-500 bg-opacity-25 text-card-foreground text-opacity-100 hover:bg-primary hover:text-primary-foreground border-border active:bg-muted active:bg-foreground active:text-background"
         )}
+        onClick={onClick}
       >
         <CardHeader>
           <CardTitle className="text-2xl">
