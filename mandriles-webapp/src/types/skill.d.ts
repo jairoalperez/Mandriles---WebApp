4a038627340dca69047d976d25d38511cc3314e3
Jairoalperez <jairoalberto98@hotmail.com>
2024-12-30 09:58:20 -0500

    created enum for skill

--- mandriles-webapp/src/types/skill.d.ts
+++ mandriles-webapp/src/types/skill.d.ts
@@ -4,3 +4,11 @@ export interface Skill {
     power: number;
     mandrilId: number;
 }
+
+export enum EPower {
+    Soft = 0,
+    Moderate = 1,
+    Intense = 2,
+    Powerful = 3,
+    Extreme = 4
+}
